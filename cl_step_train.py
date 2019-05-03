8015d20dd4af4ca822c999db91169cfc32961b27
Tin Bao Luu <tbl245@login-1-1.local>
2019-05-03 18:17:32 -0400

    okay trygit add cl_step_train.py

--- cl_step_train.py
+++ cl_step_train.py
@@ -1,14 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-'''Part 2: supervised model training
-
-Usage:
-
-    $ spark-submit supervised_train.py hdfs:/path/to/file.parquet hdfs:/path/to/save/model
-
-'''
-
-
 # We need sys to get the command line arguments
 import sys
 
@@ -21,38 +10,34 @@ from pyspark.ml.feature import StringIndexer
 from pyspark.ml import Pipeline
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.ml.evaluation import RegressionEvaluator
-# TODO: you may need to add imports here
 
-
-def main(spark, data_file, model_file):
+def main(spark, data_file, val_file, model_file):
     # Load the dataframe
     df = spark.read.parquet(data_file)
-    df_sub = df.sample(True, 0.01)
+    df = df.sample(True, 0.01)
+    val_df = spark.read.parquet(val_file)
+    val_df = df.sample(True, 0.01) 
     
     user_indexer  = StringIndexer(inputCol = "user_id", outputCol = "userNew", handleInvalid = "skip")
     track_indexer = StringIndexer(inputCol = "track_id", outputCol = "trackNew", handleInvalid = "skip")
+    
+    df = user_indexer.fit(df).transform(df)
+    df = item_indexer.fit(df).transform(df)
+    val_df = user_indexer.transform(val_df)
+    val_df = item_indexer.transform(val_df)
 
     # ALS Model 
-    als = ALS(maxIter=5, \
+    als = ALS(maxIter=5, alpha = 1, regParam = 1, rank = 10,  \
              userCol="userNew", itemCol="trackNew", ratingCol="count",\
              coldStartStrategy="drop")
     
-    pipeline = Pipeline(stages = [user_indexer, track_indexer, als]) 
-
-    paramGrid = ParamGridBuilder().addGrid(als.regParam, [0.0, 0.001, 0.01, 0.1, 0.5, 1,10]) \
-                                  .addGrid(als.alpha, [0.01, 0.1, 1, 5, 10]) \
-                                  .addGrid(als.rank, [5, 10, 20, 50, 100, 500, 1000]) \
-                                  .build()
+    model = als.fit(df) 
     
-    #cross vavildation 5-fold 
-    crossval = CrossValidator(estimator = pipeline, estimatorParamMaps = paramGrid, \
-				evaluator = RegressionEvaluator(metricName="rmse", labelCol="count",\
-                                predictionCol="prediction"), numFolds = 5)
-
-    model = crossval.fit(df_sub)
-    model = model.bestModel 
-    model.write().overwrite().save(model_file)
-
+    val_predictions = model.transform(val_df)
+    
+    evaluator = RegressionEvaluator(metricName = "rmse", labelCol = "count", predictionCol = "prediction")
+    rmse = evaluator.evaluate(val_predictions)
+    print("According to Tin, the root mean sqare error = " + str(rmse))
 
 # Only enter this block if we're in main
 if __name__ == "__main__":
