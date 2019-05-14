a3024a1b6e4b021f7444ae6616e3ff4962c4325b
Eileen Cho <echo@Eileens-MBP.fios-router.home>
2019-05-13 20:58:04 -0400

    scripts for training and evaluating basic recommender system

---
+++ EC_test.py
@@ -0,0 +1,62 @@
+'''Usage:
+
+    $ spark-submit test_file index_file model_file
+
+'''
+# We need sys to get the command line arguments
+import sys
+
+# And pyspark.sql to get the spark session
+from pyspark.sql import SparkSession
+from pyspark.ml.recommendation import ALS, ALSModel
+from pyspark.ml.feature import StringIndexer, StringIndexerModel
+from pyspark.ml import Pipeline, PipelineModel
+from pyspark.mllib.evaluation import RankingMetrics
+
+def main(spark, test_file, index_file, model_file):
+    # Load the dataframe
+    test = spark.read.parquet(test_file)
+    indexer = PipelineModel.load(index_file)
+    #transform user and track ids
+    test = indexer.transform(test)
+    #select distinct users for recommendations
+    testUsers = test.select("userNew").distinct().alias("userCol")
+    #establish "ground truth"
+    groundTruth = test.groupby("userNew").agg(F.collect_list("trackNew").alias("truth"))
+    print("created ground truth df")
+    alsmodel = ALSModel.load(model_file)
+    rec = alsmodel.recommendForUserSubset(testUsers,500)
+    print("created recs")
+    predictions = rec.join(groundTruth, rec.userNew==groundTruth.userNew, 'left')
+                
+    scoreAndLabels = predictions.select('recommendations.trackNew','truth').rdd.map(tuple)
+    metrics = RankingMetrics(scoreAndLabels)
+    precision = metrics.precisionAt(500)
+    print(f"precision at 500: {precision}")
+
+    
+    
+
+
+
+
+# Only enter this block if we're in main
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('test').getOrCreate()
+
+    # Get the filename from the command line
+    test_file = sys.argv[1]
+
+    #validation file
+
+    index_file = sys.argv[2]
+
+    # And the location to store the trained model
+    model_file = sys.argv[3]
+
+
+
+    # Call our main routine
+    main(spark, test_file, index_file, model_file)
---
+++ EC_training.py
@@ -0,0 +1,128 @@
+'''Usage:
+
+    $ spark-submit training.py training_data_path validation_data_path test_file_path model_file_path 
+
+'''
+# We need sys to get the command line arguments
+import sys
+
+# And pyspark.sql to get the spark session
+from pyspark.sql import SparkSession
+from pyspark.ml.recommendation import ALS, ALSModel
+from pyspark.ml.feature import StringIndexer, StringIndexerModel
+from pyspark.ml import Pipeline, PipelineModel
+from pyspark.sql import functions as F
+from pyspark.mllib.evaluation import RankingMetrics
+
+def main(spark, data_file, val_file, test_file, model_file, tuning = False):
+    # Load the dataframe
+    df = spark.read.parquet(data_file).
+    df.createOrReplaceTempView("df")
+    val_df = spark.read.parquet(val_file)
+    val_df.createOrReplaceTempView("val_df")
+
+    #we load in the test file just to be able to grab test_users
+    test = spark.read.parquet(test_file)
+    test.createOrReplaceTempView("test")
+    #grab only the users present in validation and test sample because whole training dataset is too large
+    df = spark.sql("SELECT * FROM df WHERE user_id IN ((SELECT user_id FROM val_df) UNION (SELECT user_id FROM test))")
+    #create and store indexer info
+    print("loaded data")
+    try:
+        #to save time, indexers have been saved. If not yet created, do indexers.
+        print("attempt to load indexers from file")
+        indexers = PipelineModel.load("./final/indexers_all")
+    except:
+        user_indexer  = StringIndexer(inputCol = "user_id", outputCol = "userNew", handleInvalid = "skip")
+        track_indexer = StringIndexer(inputCol = "track_id", outputCol = "trackNew", handleInvalid = "skip")
+        pipeline = Pipeline(stages = [user_indexer, track_indexer]) 
+        indexers = pipeline.fit(df)
+        indexers.write().overwrite().save("./final/indexers_all")
+        print("saved indexers")
+    #transform
+    df = indexers.transform(df).cache()
+    print("transformed df")
+    val_df = indexers.transform(val_df).select(["userNew","trackNew"]).repartition(1000,"userNew")
+    val_users = val_df.select("userNew").distinct().alias("userCol")
+    print("transformed val")
+    groundTruth = val_df.groupby("userNew").agg(F.collect_list("trackNew").alias("truth")).cache()
+    print("created ground truth df")
+
+    #hyperparameter tuning takes very long, so default just produces a model, set 'tuning' to True to tune
+    if tuning:
+        RegParam = [0.001, 0.1, 10,15,20]
+        Alpha = [0.001,0.01,15,45,100]
+        Rank = [15,100,300,500]
+    else:
+        RegParam = [10]
+        Alpha = [100]
+        Rank = [100]
+
+    PRECISIONS = {}
+    count = 0
+    total = len(RegParam)*len(Alpha)*len(Rank)
+    for i in RegParam:
+        for j in Alpha:
+            for k in Rank:
+
+                print(f"regParam: {i}, Alpha: {j}, Rank: {k}")
+                als = ALS(maxIter=10, regParam = i, alpha = j, rank = k, \
+                          userCol="userNew", itemCol="trackNew", ratingCol="count",\
+                          coldStartStrategy="drop",implicitPrefs=True)
+                alsmodel = als.fit(df)
+
+                rec = alsmodel.recommendForUserSubset(val_users,500)
+
+                predictions = rec.join(groundTruth, rec.userNew==groundTruth.userNew, 'left')
+                
+                scoreAndLabels = predictions.select('recommendations.trackNew','truth').rdd.map(tuple).repartition(1000)
+
+                metrics = RankingMetrics(scoreAndLabels)
+
+                precision = metrics.precisionAt(500)
+
+                PRECISIONS[precision] = [alsmodel,als]
+                count += 1
+                print(f"finished {count} of {total}")
+
+                print(precision)
+    best_precision = max(list(Precisions.keys()))
+    bestmodel,bestALS = PRECISIONS[best_precision]
+    bestmodel.write().overwrite().save(model_file)
+    bestALS.save("./final/alsFile")
+    print(f"best precision: {best_precision}, regParam: {bestALS.getRegParam}, alpha: {bestALS.getAlpha}, rank: {bestALS.getRank}")
+
+    
+
+
+
+
+# Only enter this block if we're in main
+if __name__ == "__main__":
+
+    # Create the spark session object
+    spark = SparkSession.builder.appName('training').getOrCreate()
+
+    # Get the filename from the command line
+    data_file = sys.argv[1]
+
+    #validation file
+
+    val_file = sys.argv[2]
+
+    #test file
+
+    test_file = sys.argv[3]
+
+    # And the location to store the trained model
+    model_file = sys.argv[4]
+
+    try:
+        tuning = sys.argv[5]
+    except:
+        tuning = False
+
+
+
+    # Call our main routine
+    main(spark, data_file, val_file, test_file, model_file, tuning)
